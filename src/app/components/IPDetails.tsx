ec2c9d48f7b7fc104b33a6f0ebe65142d6c77a64
imcando <irishmicolecando@gmail.com>
2025-06-24 17:25:01 +0800

    style: Add vertical divider

--- src/app/components/IPDetails.tsx
+++ src/app/components/IPDetails.tsx
@@ -8,25 +8,38 @@ interface IPAddressProps {
 const IPDetails = ({ip, location, timezone, isp} : IPAddressProps) => {
 
   return (
-    <div>
-      <div>
-        <p className="text-md font-bold text-dark-gray uppercase tracking-[0.2rem]">IP Address</p>
-        <p className="text-3xl font-semibold text-very-dark-gray">{ip}</p>
+    <div className="flex bg-white mx-10 px-5 py-6 rounded-lg justify-center content-center">
+      {/* IP Address */}
+      <div className="flex-1">
+        <p className="text-xs font-bold text-dark-gray uppercase tracking-[0.2rem] mb-3">IP Address</p>
+        <p className="text-2xl font-semibold text-very-dark-gray">{ip}</p>
+      </div>
+      
+      {/* Divider */}
+      <div className="w-px bg-gray-300 mx-4"></div>
+      
+      {/* Location */}
+      <div className="flex-1">
+        <p className="text-xs font-bold text-dark-gray uppercase tracking-[0.2rem] mb-3">Location</p>
+        <p className="text-2xl font-semibold text-very-dark-gray">{location}</p>
       </div>
 
-      <div>
-        <p className="text-md font-bold text-dark-gray uppercase tracking-[0.2rem]">Location</p>
-        <p className="text-3xl font-semibold text-very-dark-gray">{location}</p>
+      {/* Divider */}
+      <div className="w-px bg-gray-300 mx-4"></div>
+
+      {/* Timezone */}
+      <div className="flex-1">
+        <p className="text-xs font-bold text-dark-gray uppercase tracking-[0.2rem] mb-3">Timezone</p>
+        <p className="text-2xl font-semibold text-very-dark-gray">{timezone}</p>
       </div>
 
-      <div>
-        <p className="text-md font-bold text-dark-gray uppercase tracking-[0.2rem]">Timezone</p>
-        <p className="text-3xl font-semibold text-very-dark-gray">{timezone}</p>
-      </div>
+      {/* Divider */}
+      <div className="w-px bg-gray-300 mx-4"></div>
 
-      <div>
-        <p className="text-md font-bold text-dark-gray uppercase tracking-[0.2rem]">ISP</p>
-        <p className="text-3xl font-semibold text-very-dark-gray">{isp}</p>
+      {/* ISP */}
+      <div className="flex-1">
+        <p className="text-xs font-bold text-dark-gray uppercase tracking-[0.2rem] mb-3">ISP</p>
+        <p className="text-2xl font-semibold text-very-dark-gray">{isp}</p>
       </div>
     </div>
   )
